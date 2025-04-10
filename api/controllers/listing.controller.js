e101876a1a4b85f6a8c9303157b94ed36b7c5841
Rajashekar <gundarajashekarreddy@gmail.com>
2025-04-11 01:00:34 +0530

    updated listing.controller.js

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -90,7 +90,7 @@ export const getListings=async (req,res,next)=>{
         }
 
         let type=req.query.type 
-        if (type===undefined || type==='false'){
+        if (type===undefined || type==='false' || type==='all'){
             type={$in:['sale','rent']}
         }
         const searchTerm=req.query.searchTerm || ''
